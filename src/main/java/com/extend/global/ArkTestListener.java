8d89054fc17802c5700ecfce3ae0c0c759cfef51
arjun.mannambeth <arjun.mannambeth@nutanix.com>
2022-02-02 17:54:01 +0530

    added superset logic

--- src/main/java/com/extend/global/ArkTestListener.java
+++ src/main/java/com/extend/global/ArkTestListener.java
@@ -7,6 +7,7 @@ import org.testng.xml.XmlSuite;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArkTestListener implements ITestListener, IAlterSuiteListener {
 
@@ -16,14 +17,17 @@ public class ArkTestListener implements ITestListener, IAlterSuiteListener {
         String commitId = System.getProperty("commitId", "");
         if (commitId != null && commitId.length() > 0) {
             List<String> words = new CommitReader().getCommitKeywords(commitId);
-            List<String> groupMasterList = Arrays.asList("login", "signup", "payment");
+            List<String> groupMasterList = Arrays.asList("login", "signup", "payment", "cancelplan", "managrplan", "order", "planexpiry", "plan", "promo", "tax");
             //String[] words = commitMsg.split(" ");
             List<String> groups = new ArrayList<>();
             for (String word : words) {
                 word = word.toLowerCase();
                 if (groupMasterList.contains(word))
                     groups.add(word);
+                if(word.equals("superset"))
+                    return;
             }
+            groups = groups.stream().distinct().collect(Collectors.toList());
             for (XmlSuite suite : suiteList) {
                 suite.setIncludedGroups(groups);
                 System.out.println("Predicted test case groups : " + groups);
